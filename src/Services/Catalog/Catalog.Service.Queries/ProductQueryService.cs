ba52d36668f95888211c909a434ce113b2d0abfa
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-04 18:01:09 -0500

    common projects added for pagination

---
+++ src/Services/Catalog/Catalog.Service.Queries/ProductQueryService.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Persistence.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Service.Queries
+{
+    public class ProductQueryService
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductQueryService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }        
+    }
+}
