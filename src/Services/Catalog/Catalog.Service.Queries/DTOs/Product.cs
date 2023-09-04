5261a16f59efea4b06de48c249b9127d46165e62
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-04 17:22:37 -0500

    dtos added

---
+++ src/Services/Catalog/Catalog.Service.Queries/DTOs/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Service.Queries.DTOs
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        //Propiedades de navegación
+        public ProductInStockDto Stock { get; set; }
+    }
+}
