3d6842da6bd5535016763a52cbc8ad9abb09362d
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-08 11:48:53 -0500

    persistence classes added

---
+++ src/Services/Customer/Customer.Persistence.Database/Configuration/ClientConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Customer.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Customer.Persistence.Database.Configuration
+{
+    public class ClientConfiguration
+    {
+        public ClientConfiguration(EntityTypeBuilder<Client> entityBuilder)
+        {
+            entityBuilder.HasIndex(x => x.ClientId);
+            entityBuilder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+
+            SeedData(entityBuilder);
+        }
+
+        private void SeedData(EntityTypeBuilder entityTypeBuilder)
+        {
+            List<Client> clients = new List<Client>();
+            
+            for(int i = 1; i<= 100; i++)
+            {
+                clients.Add(new Client
+                {
+                    Name = $"Cliente {i}",
+                });
+            }
+
+            entityTypeBuilder.HasData(clients);
+        }
+    }
+}
