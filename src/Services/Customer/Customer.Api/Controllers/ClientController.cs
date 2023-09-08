6ac9d42e7b26af36779051319a81ea4614b7417b
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-08 13:07:32 -0500

    cqrs for reading completed

---
+++ src/Services/Customer/Customer.Api/Controllers/ClientController.cs
@@ -0,0 +1,40 @@
+﻿using Customer.Service.Queries;
+using Customer.Service.Queries.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Service.Common.Collection;
+
+namespace Customer.Api.Controllers
+{
+    [ApiController]
+    [Route("clients")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> logger;
+        private readonly IClientQueryService clientQueryService;
+
+        public ClientController(ILogger<ClientController> logger, IClientQueryService clientQueryService)
+        {
+            this.logger = logger;
+            this.clientQueryService = clientQueryService;
+        }
+
+        //-- Route: /clients
+        [HttpGet]
+        public async Task<DataCollection<ClientDto>> GetAll(int page = 1, int take = 10, string? ids = null)
+        {
+            IEnumerable<int> clientIds = null;
+
+            if (!string.IsNullOrEmpty(ids))
+                clientIds = ids.Split(',').Select(x => Convert.ToInt32(x));
+
+            return await clientQueryService.GetAllAsync(page, take, clientIds);
+        }
+
+        //-- Route: /clients/1
+        [HttpGet("{id:int}")]
+        public async Task<ClientDto> Get(int id)
+        {
+            return await clientQueryService.GetAsync(id);
+        }
+    }
+}
