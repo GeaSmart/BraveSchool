8fd31e1ff0b91e2b423ca063c5286e69e73e7aae
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-11 12:45:47 -0500

    order create event handler handle method added, ready to code it

---
+++ src/Services/Order/Order.Service.EventHandlers/OrderCreateEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Order.Persistence.Database;
+using Order.Service.EventHandlers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Service.EventHandlers
+{
+    public class OrderCreateEventHandler : INotificationHandler<OrderCreateCommand>
+    {
+        private readonly ApplicationDbContext context;
+        private readonly ILogger<OrderCreateEventHandler> logger;
+
+        public OrderCreateEventHandler(ApplicationDbContext context, ILogger<OrderCreateEventHandler> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        public Task Handle(OrderCreateCommand notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
