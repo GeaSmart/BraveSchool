5a7f842236df2387d0583884454200d383512559
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-13 22:29:33 -0500

    identity migration done

--- src/Services/Identity/Identity.Api/Program.cs
+++ src/Services/Identity/Identity.Api/Program.cs
@@ -1,6 +1,35 @@
+using Identity.Domain;
+using Identity.Persistence.Database;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Configuring DB context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("defaultConnection"),
+        x => x.MigrationsHistoryTable("__EFMigrationsHistory", "Identity")
+    )
+);
+
+// Identity
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Identity configuration
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
