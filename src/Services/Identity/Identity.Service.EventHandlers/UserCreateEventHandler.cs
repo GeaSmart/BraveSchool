ffbe5c65e0b2c72e760e6d0fdaca28486960d719
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-14 22:47:56 -0500

    identity api and service.eventhandlers added

---
+++ src/Services/Identity/Identity.Service.EventHandlers/UserCreateEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Identity.Domain;
+using Identity.Service.EventHandlers.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Service.EventHandlers
+{
+    public class UserCreateEventHandler : IRequestHandler<UserCreateCommand, IdentityResult>
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserCreateEventHandler(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<IdentityResult> Handle(UserCreateCommand notification, CancellationToken cancellationToken)
+        {
+            var entry = new ApplicationUser
+            {
+                FirstName = notification.FirstName,
+                LastName = notification.LastName,
+                Email = notification.Email,
+                UserName = notification.Email
+            };
+
+            return await userManager.CreateAsync(entry, notification.Password);
+        }
+    }
+}
