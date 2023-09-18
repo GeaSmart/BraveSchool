8a1c6899cb6f554513c2301d907461dfe9c48cec
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-18 15:42:52 -0500

    ocelot configured in Gateway API

---
+++ src/Gateway/Gateway.Api/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Gateway.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController : ControllerBase
+    {
+        public DefaultController()
+        {
+
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var project = Assembly.GetEntryAssembly().GetName().Name;
+            return $"{project} running...";
+        }
+    }
+}
