cc7d6d1b2410b5920e858b11f2c634aebf1209a4
Gerson Azabache <gerson_dunk@hotmail.com>
2023-09-25 16:27:03 -0500

    ocelot update 5

--- src/Gateway/Gateway.Api/Handlers/HeaderDelegatingHandler.cs
+++ src/Gateway/Gateway.Api/Handlers/HeaderDelegatingHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 
 namespace Gateway.Api.Handlers
 {
@@ -13,16 +14,28 @@ namespace Gateway.Api.Handlers
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            IEnumerable<string> headerValues;
-            if (request.Headers.TryGetValues("access_token", out headerValues))
+            if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated && _httpContextAccessor.HttpContext.Request.Headers.ContainsKey("Authorization"))
             {
-                string accessToken = headerValues.First();
+                var token = _httpContextAccessor.HttpContext.Request.Headers["Authorization"].ToString();
 
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-                request.Headers.Remove("access_token");
+                if (!string.IsNullOrEmpty(token))
+                {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);                    
+                }
             }
 
-            return await base.SendAsync(request, cancellationToken);
+            return await base.SendAsync(request,cancellationToken);
+
+            //IEnumerable<string> headerValues;
+            //if (request.Headers.TryGetValues("access_token", out headerValues))
+            //{
+            //    string accessToken = headerValues.First();
+
+            //    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            //    request.Headers.Remove("access_token");
+            //}
+
+            //return await base.SendAsync(request, cancellationToken);
         }
     }
 }
